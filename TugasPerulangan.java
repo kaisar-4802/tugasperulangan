99523569a9d80a942591264fd40d891344adfc1f
Patrick Telnoni <ptelnoni89@gmail.com>
2024-10-13 04:10:49 +0700

    tugas perulangan

---
+++ TugasPerulangan.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class TugasPerulangan{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int stokAwal = input.nextInt();
+        int pembelianPerhari = input.nextInt();
+        
+
+        
+        System.out.println(stokAwal);
+    }
+}
---
+++ TugasPerulangan2.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+
+public class TugasPerulangan2{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N  = input.nextInt();
+    }
+}
